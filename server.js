5a840fe3d869fe84cdc28e0bee41da18b9346dd4
azzo133456-cmd <azzo133456@gmail.com>
2026-03-21 18:58:41 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 const app = express();
 app.use(cors());
 
-// 讀取 JSON 檔案
-const lamps = JSON.parse(fs.readFileSync("./lamps.json", "utf8"));
+const db = await open({
+  filename: "./lamps.db",
+  driver: sqlite3.Database
+});
 
-app.get("/lamp/:id", (req, res) => {
+app.get("/lamp/:id", async (req, res) => {
   const id = req.params.id;
-  const lamp = lamps.find(l => l["路燈編號"] === id);
+
+  const lamp = await db.get("SELECT * FROM lamps WHERE id = ?", [id]);
 
   if (!lamp) return res.status(404).json({ error: "查無此路燈編號" });
 
   res.json({
-    id: lamp["路燈編號"],
-    address: lamp["詳細位置"],
-    lat: lamp["緯度"],
-    lng: lamp["經度"],
-    nav: `https://www.google.com/maps/dir/?api=1&destination=${lamp["緯度"]},${lamp["經度"]}`
+    id: lamp.id,
+    address: lamp.address,
+    lat: lamp.lat,
+    lng: lamp.lng,
+    nav: `https://www.google.com/maps/dir/?api=1&destination=${lamp.lat},${lamp.lng}`
   });
 });
 
-// Render 會提供 PORT，不能寫死 3000
-app.listen(process.env.PORT || 3000, () =>
-  console.log("API running")
-);
+app.listen(process.env.PORT || 3000, () => console.log("API running"));
